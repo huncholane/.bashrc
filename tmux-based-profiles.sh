868a756599f6ee63b7c168edb979e2b1adb87bc1
huncholane <113503551+huncholane@users.noreply.github.com>
2025-03-11 01:01:24 -0700

    Update tmux-based-profiles.sh

--- tmux-based-profiles.sh
+++ tmux-based-profiles.sh
@@ -1,5 +1,6 @@
 # Add bash profile based on tmux session name
 if [ -n "$TMUX" ]; then
+    TMUX_SESSION=$(tmux display-message -p '#S')
     case "$TMUX_SESSION" in
         "fb")
             source ~/.fb_bashrc
