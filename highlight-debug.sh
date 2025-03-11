cdd1b431b18bdbbfb0c7a26e3421e1126a650d84
huncholane <113503551+huncholane@users.noreply.github.com>
2025-03-11 00:08:00 -0700

    Update highlight-debug.sh

--- highlight-debug.sh
+++ highlight-debug.sh
@@ -6,11 +6,21 @@ highlight() {
     fi
     results=$(eval "$BASH_COMMAND")
     status=$?
-    echo -e "$results" | sed "s|^.*$(pwd).*$|$(printf '\033[0;34m')\0$(printf '\033[0m')|g"
+    if [ -n "$results" ]; then # Prevent highlighting if no results
+        echo -e "$results" | sed "s|^.*$(pwd).*$|$(printf '\033[0;34m')\0$(printf '\033[0m')|g"
+    fi
 
     if [ $status=0 ]; then
         return 69 # 69 becomes your new success code
     fi
     return $status # Return original error code
 }
-trap highlight DEBUG
+uh() { # Unset the highlight trap
+    echo -e "\033[31mUnsetting highlight trap\033[0m"
+    trap - DEBUG
+}
+hh() { # Set the highlight trap
+    echo -e "\033[32mSetting highlight trap\033[0m"
+    trap highlight DEBUG
+}
+trap highlight DEBUG # Set the highlight trap by default
