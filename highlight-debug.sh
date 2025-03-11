7dec4c3c2a6ba3746cc7ccba64c284998e462688
huncholane <113503551+huncholane@users.noreply.github.com>
2025-03-10 23:48:55 -0700

    Update highlight-debug.sh

--- highlight-debug.sh
+++ highlight-debug.sh
@@ -2,9 +2,16 @@
 shopt -s extdebug
 highlight() {
     if [ "$BASH_LOADED" = false ]; then
-        return 0
+        return 0 # Return 0 lets the command run how it normally would
     fi
-    eval "$BASH_COMMAND" | sed "s|^.*$(pwd).*$|$(printf '\033[0;34m')\0$(printf '\033[0m')|g"
-    return 1
+    results=$(eval "$BASH_COMMAND")
+    status=$?
+    echo -e "$results" | sed "s|^.*$(pwd).*$|$(printf '\033[0;34m')\0$(printf '\033[0m')|g"
+
+    if [ $status -ne 0 ]; then
+        return $status
+    else
+        return 69 # 69 becomes your new success code
+    fi
 }
 trap highlight DEBUG
