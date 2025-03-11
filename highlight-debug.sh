26c6e8e9cf4f5c8f6326189dd5a8c05e322034f5
huncholane <113503551+huncholane@users.noreply.github.com>
2025-03-10 23:45:47 -0700

    Update highlight-debug.sh

--- highlight-debug.sh
+++ highlight-debug.sh
@@ -1,7 +1,10 @@
 # Highlight the current directory for all commands
+shopt -s extdebug
 highlight() {
+    if [ "$BASH_LOADED" = false ]; then
+        return 0
+    fi
     eval "$BASH_COMMAND" | sed "s|^.*$(pwd).*$|$(printf '\033[0;34m')\0$(printf '\033[0m')|g"
     return 1
 }
-shopt -s extdebug
 trap highlight DEBUG
