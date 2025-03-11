9a9b7ba5399f3f77a9c399152829dcea5e015df9
huncholane <113503551+huncholane@users.noreply.github.com>
2025-03-11 02:06:36 -0700

    Update highlight-debug.sh

--- highlight-debug.sh
+++ highlight-debug.sh
@@ -1,7 +1,8 @@
 # Highlight the current directory for all commands
+# WARNING: This changes the success status code to 69
 shopt -s extdebug
 highlight() {
-    if [ "$BASH_LOADED" = false ]; then
+    if [ "$BASH_LOADED" = false ] || [ "$BASH_COMMAND" = "uh" ] || [ "$BASH_COMMAND" = "hh" ] || [[ "$BASH_COMMAND" =~ "trap" ]]; then
         return 0 # Return 0 lets the command run how it normally would
     fi
     results=$(eval "$BASH_COMMAND" 2>&1)
@@ -13,6 +14,7 @@ highlight() {
     if [ $status=0 ]; then
         return 69 # 69 becomes your new success code
     fi
+    echo "status: $status"
     return $status # Return original error code
 }
 uh() { # Unset the highlight trap
@@ -23,4 +25,4 @@ hh() { # Set the highlight trap
     echo -e "\033[32mSetting highlight trap\033[0m"
     trap highlight DEBUG
 }
-trap highlight DEBUG # Set the highlight trap by default
+# trap highlight DEBUG # Set the highlight trap by default
